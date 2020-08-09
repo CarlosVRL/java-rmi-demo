219e85f31cfa742fdceca9385fb92dce81640d0a
CarlosVRL <carlos.varela.eng@gmail.com>
2020-08-08 22:11:51 -0500

    Add output for client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -8,7 +8,9 @@ public class Client {
         try {
             Registry registry = LocateRegistry.getRegistry(host);
             Hello stub = (Hello) registry.lookup("Hello");
+            System.out.println("Sending RMI Request");
             stub.printMsg();
+            System.out.println("RMI Request Complete");
         } catch (Exception e) {
             System.out.println("Client exception: " + e.toString());
             e.printStackTrace();
